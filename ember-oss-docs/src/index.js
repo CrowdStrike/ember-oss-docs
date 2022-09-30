b47874a8858afb90e0ccaee18f35f89177937b0b
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-09-29 22:44:45 -0400

    fix: top-level exports for laziness

---
+++ ember-oss-docs/src/index.js
@@ -0,0 +1,5 @@
+export { default as SubSection } from './components/sub-section';
+export { default as ExternalLink } from './components/external-link';
+export { default as CrowdStrike } from './components/crowd-strike';
+export { default as Link } from './components/toucan/link/index';
+export { default as Icon } from './components/toucan/icon/index';
