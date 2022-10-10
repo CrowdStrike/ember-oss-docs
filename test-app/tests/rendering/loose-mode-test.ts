45eb76c539dccd017e6b7ad63ff3a1ae46853516
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-10 10:37:47 -0400

    chore(ts): drop to TS 4.7, due to: https://github.com/typed-ember/glint/issues/406

--- test-app/tests/rendering/loose-mode-test.ts
+++ test-app/tests/rendering/loose-mode-test.ts
@@ -1,6 +1,3 @@
-import '@glint/environment-ember-loose';
-import '@crowdstrike/ember-oss-docs/glint';
-
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
@@ -11,17 +8,17 @@ module('Rendering | all - loose mode', function (hooks) {
 
   test('all components can be rendered', async function (assert) {
     await render(hbs`
-        <App>
-          <:topBar>x</:topBar>
-          <:content></:content>
-          <:footer></:footer>
-        </App>
-        <ContentSection>
-          <:header>x</:header>
-          <:content>x</:content>
-          <:footer>x</:footer>
-        </ContentSection>
-      `);
+      <App>
+        <:topBar>x</:topBar>
+        <:content>x</:content>
+        <:footer>x</:footer>
+      </App>
+      <ContentSection>
+        <:header>x</:header>
+        <:content>x</:content>
+        <:footer>x</:footer>
+      </ContentSection>
+    `);
 
     assert.true(true, 'we did it');
   });
--- test-app/types/glint-registry.d.ts
+++ test-app/types/glint-registry.d.ts
@@ -5,8 +5,8 @@ import '@crowdstrike/ember-oss-docs/glint';
 
 // import type { ComponentLike, HelperLike, ModifierLike } from "@glint/template";
 
-declare module "@glint/environment-ember-loose/registry" {
-  export default interface Registry {
-    // Local types here
-  }
-}
+// declare module "@glint/environment-ember-loose/registry" {
+//   export default interface Registry {
+//     // Local types here
+//   }
+// }
