30c964b5b4205131c4eee199c2dc1c9de782aae9
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-11 11:33:03 -0400

    chore(tests): add more

--- test-app/tests/rendering/loose-mode-test.ts
+++ test-app/tests/rendering/loose-mode-test.ts
@@ -3,10 +3,20 @@ import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 
+/**
+ * Disclaimer, these tests aren't worth much outside of "they don't error" (including via Glint)
+ *
+ * This is due to the temporary nature of some of these components, and also
+ * the lack of docs (a docs app would have better tests)
+ */
 module('Rendering | all - loose mode', function (hooks) {
   setupRenderingTest(hooks);
 
   test('all components can be rendered', async function (assert) {
+    this.setProperties({
+      noop: () => {},
+    });
+
     await render(hbs`
       <App>
         <:topBar>x</:topBar>
@@ -18,6 +28,25 @@ module('Rendering | all - loose mode', function (hooks) {
         <:content>x</:content>
         <:footer>x</:footer>
       </ContentSection>
+      <CrowdStrike />
+      <ExternalLink @href="#">x</ExternalLink>
+      <Hero>x</Hero>
+      <Main>x</Main>
+      <SubSection>
+        <:header>x</:header>
+        <:content>x</:content>
+        <:footer>x</:footer>
+      </SubSection>
+      <TopBar>
+        <:logo>x</:logo>
+        <:title>x</:title>
+        <:links>x</:links>
+      </TopBar>
+
+      <Toucan::Button @onClick={{this.noop}}>x</Toucan::Button>
+      <Toucan::Icon @path="micro/caret-right-16" />
+      <Toucan::Input />
+      <Toucan::Link @href="#">x</Toucan::Link>
     `);
 
     assert.true(true, 'we did it');
--- test-app/tests/rendering/strict-mode-test.gts
+++ test-app/tests/rendering/strict-mode-test.gts
@@ -4,10 +4,18 @@ import { render } from '@ember/test-helpers';
 
 import * as OSS from '@crowdstrike/ember-oss-docs';
 
+/**
+ * Disclaimer, these tests aren't worth much outside of "they don't error" (including via Glint)
+ *
+ * This is due to the temporary nature of some of these components, and also
+ * the lack of docs (a docs app would have better tests)
+ */
 module('Rendering | all - strict mode', function(hooks) {
   setupRenderingTest(hooks);
 
   test('all components can be rendered', async function (assert) {
+    const noop = () => {};
+
     await render(
       <template>
         <OSS.App>
@@ -20,6 +28,26 @@ module('Rendering | all - strict mode', function(hooks) {
           <:content>x</:content>
           <:footer>x</:footer>
         </OSS.ContentSection>
+        <OSS.CrowdStrike />
+        <OSS.ExternalLink @href="#">x</OSS.ExternalLink>
+        <OSS.Hero>x</OSS.Hero>
+        <OSS.Main>x</OSS.Main>
+        <OSS.SubSection>
+          <:header>x</:header>
+          <:content>x</:content>
+          <:footer>x</:footer>
+        </OSS.SubSection>
+        <OSS.TopBar>
+          <:logo>x</:logo>
+          <:title>x</:title>
+          <:links>x</:links>
+        </OSS.TopBar>
+
+        <OSS.Button @onClick={{noop}}>x</OSS.Button>
+        <OSS.Icon @path="micro/caret-right-16" />
+        <OSS.Input />
+        <OSS.Link @href="#">x</OSS.Link>
+
       </template>
     );
 
