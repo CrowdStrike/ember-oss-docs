2235ee4ace4ef27010e0c6f8ec112926bc017b2b
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-11 10:55:35 -0400

    chore(lint): fix

--- test-app/tests/rendering/loose-mode-test.ts
+++ test-app/tests/rendering/loose-mode-test.ts
@@ -1,9 +1,9 @@
+import '@crowdstrike/ember-oss-docs/glint';
+
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-
-import '@crowdstrike/ember-oss-docs/glint';
 
 module('Rendering | all - loose mode', function (hooks) {
   setupRenderingTest(hooks);
