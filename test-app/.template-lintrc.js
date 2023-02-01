8b6f97c5f73fbd188088375f998643bf8a69eebc
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-02-01 15:58:44 -0500

    chore: update linting

--- test-app/.template-lintrc.js
+++ test-app/.template-lintrc.js
@@ -2,4 +2,12 @@
 
 module.exports = {
   extends: 'recommended',
+  overrides: [
+    {
+      files: ['**/*.gts', '**/*.gjs'],
+      rules: {
+        'no-implicit-this': 'off',
+      },
+    },
+  ],
 };
